a8e0a98ce928fd37c2c4e17f26b3f93f8b355e3b
Derek Chou <derekchou3704@gmail.com>
2024-08-29 15:02:18 +0800

    sync trashCounter

--- Assets/Scripts/KitchenObject.cs
+++ Assets/Scripts/KitchenObject.cs
@@ -57,9 +57,11 @@ public class KitchenObject : NetworkBehaviour {
     }
 
     public void DestroySelf() {
-        kitchenObjectParent.ClearKitchenObject();
-
         Destroy(gameObject);
+    }
+
+    public void ClearKitchenObjectOnParent() {
+        kitchenObjectParent.ClearKitchenObject();
     }
 
     public bool TryGetPlate(out PlateKitchenObject plateKitchenObject) {
@@ -79,4 +81,7 @@ public class KitchenObject : NetworkBehaviour {
         
     }
 
+    public static void DestroyKitchenObject(KitchenObject kitchenObject) {
+        KitchenGameMultiplayer.Instance.DestroyKitchenObject(kitchenObject);
+    }
 }
